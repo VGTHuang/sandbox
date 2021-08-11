131c8392641587417838df013be76955b0e0743a
VGTHuang <1187184299@qq.com>
2021-08-11 16:54:17 +0800

    20210811 front end test

---
+++ frontend_test/vue-test/src/components/SocketTest.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button @click="connectToSocket">connect to socket</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            backend: 'asdf'
+        }
+    },
+    methods: {
+        connectToSocket() {
+            console.log('connect')
+        }
+    }
+
+}
+</script>
