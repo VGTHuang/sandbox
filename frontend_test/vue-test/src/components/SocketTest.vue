08705c1eb59886ccb04dc9872db8bfaa80c94650
VGTHuang <1187184299@qq.com>
2021-08-13 21:06:26 +0800

    20210813

--- frontend_test/vue-test/src/components/SocketTest.vue
+++ frontend_test/vue-test/src/components/SocketTest.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         <button @click="connectToSocket">connect to socket</button>
+        <button @click="disconnectSocket">disconnect socket</button>
+        <button @click="sendMessage">send message</button>
     </div>
 </template>
 
@@ -8,12 +10,32 @@
 export default {
     data() {
         return {
-            backend: 'asdf'
+            backend: 'ws://127.0.0.1:5000/api',
+            websocket: undefined
         }
     },
     methods: {
         connectToSocket() {
+            let self = this
             console.log('connect')
+            this.websocket = new WebSocket(this.backend)
+            this.websocket.onopen = function() {
+                console.log('open', self.websocket)
+                self.websocket.send('New participant joined');
+            };
+            this.websocket.onmessage = function(e) {
+                console.log('message received', e)
+            };
+        },
+        disconnectSocket() {
+            if(this.websocket) {
+                this.websocket.close()
+            }
+        },
+        sendMessage() {
+            this.websocket.send(JSON.stringify({
+                's': 'asdasdasd'
+            }));
         }
     }
 
